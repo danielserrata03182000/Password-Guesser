ecffa5e59425a823f46ca9e556079e126bb76de5
Daniel Serrata <daniel.s.student@lyonscommunityschool.org>
2017-03-15 14:29:07 +0000

    create password guesser

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var password = "password";
+
+$("#enter").click(function() {
+
+
+if (password === $("#input").val())
+{
+    
+    $("#apples").append("congragulastions you played yourself");
+    alert('it worked');
+    
+}
+
+
+var name = $("#input").val();
+
+});
+});
